cecb5887fc7a4214e047e8de2255aedbcc16c37b
Ihor Reznichenko <igor7210ak@gmail.com>
2024-09-19 13:48:09 +0300

    feat: add camera movement logic

--- Assets/Scripts/Movement/CameraMovementController.cs
+++ Assets/Scripts/Movement/CameraMovementController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace CubePuzzle.Movement
@@ -8,10 +9,16 @@ namespace CubePuzzle.Movement
 		private Transform _centerPoint;
 
 		[SerializeField]
-		private float _sensitivity;
+		private float _moveSensitivity;
+		
+		[SerializeField]
+		private float _zoomSensitivity;
 
 		[SerializeField]
-		private float _smoothTime;
+		private float _moveSmoothTime;
+		
+		[SerializeField]
+		private float _zoomSmoothTime;
 
 		[SerializeField]
 		private float _maxDistance;
@@ -26,15 +33,23 @@ namespace CubePuzzle.Movement
 		private bool _isEnabled = false;
 
 		private Transform _cameraTransform;
-		
-		private float _currentDistance;
+
+		private float _lastZoomGestureDistance = 0;
 
 		private float _rotationY;
 		private float _rotationX;
 
+		#region Rotation Smooth
 		private Vector3 _currentRotation;
 		private Vector3 _nextRotation;
-		private Vector3 _smoothVelocity = Vector3.zero;
+		private Vector3 _smoothMoveVelocity = Vector3.zero;
+		#endregion
+
+		#region ZoomSmooth
+		private float _currentDistance;
+		private float _nextDistance;
+		private float _smoothZoomVelocity = 0;
+		#endregion
 
 		private void Awake()
 		{
@@ -46,36 +61,81 @@ namespace CubePuzzle.Movement
 			_rotationX = _cameraTransform.localEulerAngles.x;
 			_rotationY = _cameraTransform.localEulerAngles.y;
 			_currentDistance = _maxDistance;
+
 			_nextRotation = _cameraTransform.localEulerAngles;
+			_nextDistance = _currentDistance;
 		}
 
 		private void Update()
 		{
-			if (_isEnabled && Input.touchCount > 0)
+			if (_isEnabled)
 			{
-				Touch touch = Input.GetTouch(0);
+				int touchCount = Input.touchCount;
 
-				Vector2 touchMovement = touch.deltaPosition;
-
-				float xDelta = touchMovement.x * _sensitivity;
-
-				float yDelta = touchMovement.y * _sensitivity;
-
-				_rotationX -= yDelta;
-				_rotationY += xDelta;
-
-				_rotationX = Mathf.Clamp(_rotationX, -_xAxisDeltaRotation, _xAxisDeltaRotation);
-
-				_nextRotation = new Vector3(_rotationX, _rotationY);
-
+				if (touchCount > 0)
+				{
+					if (touchCount == 1)
+					{
+						_lastZoomGestureDistance = 0;
+						CalculateRotation();
+					}
+					else
+					{
+						CalculateCameraDistance();
+					}
+				}
+				else
+				{
+						_lastZoomGestureDistance = 0;
+				}
 			}
 
-			UpdateRotation();
+			UpdateRotationAndDistance();
 		}
 
-		private void UpdateRotation()
+		private void CalculateCameraDistance()
 		{
-			_currentRotation = Vector3.SmoothDamp(_currentRotation, _nextRotation, ref _smoothVelocity, _smoothTime);
+			Touch first = Input.GetTouch(0);
+			Touch second = Input.GetTouch(1);
+
+			float distance = (first.position - second.position).magnitude;
+
+			if(_lastZoomGestureDistance == 0)
+			{
+				_lastZoomGestureDistance = distance;
+			}
+
+			float delta = distance - _lastZoomGestureDistance;
+
+			_lastZoomGestureDistance = distance;
+
+			_nextDistance -= delta * _zoomSensitivity;
+
+			_nextDistance = Mathf.Clamp(_nextDistance, _minDistance, _maxDistance);
+		}
+
+		private void CalculateRotation()
+		{
+			Touch touch = Input.GetTouch(0);
+
+			Vector2 touchMovement = touch.deltaPosition;
+
+			float xDelta = touchMovement.x * _moveSensitivity;
+
+			float yDelta = touchMovement.y * _moveSensitivity;
+
+			_rotationX -= yDelta;
+			_rotationY += xDelta;
+
+			_rotationX = Mathf.Clamp(_rotationX, -_xAxisDeltaRotation, _xAxisDeltaRotation);
+
+			_nextRotation = new Vector3(_rotationX, _rotationY);
+		}
+
+		private void UpdateRotationAndDistance()
+		{
+			_currentDistance = Mathf.SmoothDamp(_currentDistance, _nextDistance, ref _smoothZoomVelocity, _zoomSmoothTime);
+			_currentRotation = Vector3.SmoothDamp(_currentRotation, _nextRotation, ref _smoothMoveVelocity, _moveSmoothTime);
 
 			_cameraTransform.localEulerAngles = _currentRotation;
 			_cameraTransform.position = _centerPoint.position - _cameraTransform.forward * _currentDistance;
